26b1ddd379a84a87fea4ff11985825b3a67026a2
John Brooks <john@fastquake.com>
2018-01-08 16:11:23 -0500

    Refactor plugin code into classes
    Create a new FSTree class for the main fstree code and shared variables
    (which were previously global), and wrap it in a new FSTreePlugin class,
    which will now contain the register() function and all the signal
    receivers.

--- __init__.py
+++ __init__.py
@@ -1,1 +1,47 @@
-from .fstree import register
+from pelican import signals
+
+from .dirlist import DirListGenerator
+from .fstree import FSTree
+
+class FSTreePlugin:
+    def __init__(self):
+        self.fstree = None
+        self.settings = None
+        self.dlgen = None
+
+    def try_init_fstree(self):
+        if self.fstree is None:
+            if self.settings is not None and self.dlgen is not None:
+                self.fstree = FSTree(self.settings, self.dlgen)
+
+    def init(self, pelican):
+        self.settings = pelican.settings
+
+    def add_dirlist_generator(self, pelican):
+        return DirListGenerator
+
+    def get_dirlist_generator(self, generator):
+        if isinstance(generator, DirListGenerator):
+            self.dlgen = generator
+
+    def add_nodes(self, generators, outputs):
+        self.try_init_fstree()
+        assert self.fstree
+        self.fstree.add_nodes(generators, outputs)
+
+    def add_dir(self, content):
+        self.try_init_fstree()
+        assert self.fstree
+        self.fstree.add_dir(content)
+
+    def register(self):
+        signals.initialized.connect(self.init)
+        signals.get_generators.connect(self.add_dirlist_generator)
+        signals.generator_init.connect(self.get_dirlist_generator)
+        signals.all_generators_finalized.connect(self.add_nodes)
+        signals.content_object_init.connect(self.add_dir)
+
+def register():
+    global _plugin # If this goes out of scope, it will not receive signals
+    _plugin = FSTreePlugin()
+    _plugin.register()
--- fstree.py
+++ fstree.py
@@ -1,150 +1,133 @@
 import posixpath as osp
 from functools import partial
 
-from pelican import signals
 from pelican import contents
 from pelican.outputs import HTMLOutput
 
 from .fsnode import Node, NodePrecursor as NP
 from .render_tree import render_tree, render_tree_ancestors
-from .dirlist import DirListGenerator
 from .utils import normalize_path, split_path
 
-settings = None
-dlgen = None
-precursors = []
+class FSTree:
+    def __init__(self, settings, dlgen):
+        self.settings = settings
+        self.dlgen = dlgen
+        self.precursors = []
 
-"""Initialize a Node with a parent (optional), name, and Output object.
+    """Initialize a Node with a parent (optional), name, and Output object.
 
-The new Node is added to the parent's list of children, and a reference
-to it is stored in the Output object's "node" template variable.
-"""
-def init_node(parent, name, output):
-    node = Node(parent, name, output)
-    node.render_tree = partial(render_tree, node, settings)
-    node.render_tree_ancestors = partial(render_tree_ancestors, node, settings)
-    output.template_vars['node'] = node
-    if parent:
-        parent.add_child(node)
-    return node
+    The new Node is added to the parent's list of children, and a reference
+    to it is stored in the Output object's "node" template variable.
+    """
+    def init_node(self, parent, name, output):
+        node = Node(parent, name, output)
+        node.render_tree = partial(render_tree, node, self.settings)
+        node.render_tree_ancestors = partial(render_tree_ancestors, node, self.settings)
+        output.template_vars['node'] = node
+        if parent:
+            parent.add_child(node)
+        return node
 
-"""Generates the tree and links each Output object to a Node.
+    """Generates the tree and links each Output object to a Node.
 
-Nodes will be stored in the corresponding Output object's "node"
-template variable by init_node().
-"""
-def add_nodes(generators, outputs):
-    # Convert outputs to precursors
-    for output in outputs:
-        if type(output) is HTMLOutput:
-            precursors.append(NP(normalize_path(output.path), output))
+    Nodes will be stored in the corresponding Output object's "node"
+    template variable by init_node().
+    """
+    def add_nodes(self, generators, outputs):
+        # Convert outputs to precursors
+        for output in outputs:
+            if type(output) is HTMLOutput:
+                self.precursors.append(NP(normalize_path(output.path), output))
 
-    # Test for duplicate paths
-    paths = [p.path for p in precursors]
-    try:
-        assert len(paths) == len(set(paths)), "we got dupes"
-    except AssertionError as e:
-        e.args += (paths,)
-        raise
-
-    # Sort precursors in ascending order of number of path components
-    precursors.sort(key=lambda item: len(item.components))
-
-    # Create root node for precursor with first (shortest) path
-    root = init_node(None, precursors[0].name, precursors[0].output)
-    nodes = [[(precursors[0].components, root)]] # Indexed by depth, nodes[0]
-                                                 # contains only ('/', root).
-    precursors.pop(0)
-
-    # Create the rest of the nodes
-    for precursor in precursors:
-        depth = len(precursor.components)-1
-
-        if len(nodes) <= depth:
-            nodes.append([])
-
-        parent = None
-        for p in nodes[depth-1]:
-            if p[0] == precursor.components[:-1]:
-                parent = p[1]
-
-        if not parent:
-            # Go up until we find an ancestor that exists
-            for level in range(depth-1, -1, -1):
-                for p in nodes[level]:
-                    if p[0] == precursor.components[:level+1]:
-                        # Found an ancestor. The nesting is getting a
-                        # bit ridiculous, but bear with me. Now we're
-                        # going to create a directory listing for each
-                        # missing level.
-                        _parent = p[1]
-                        for missing in range(level+1, depth):
-                            _components = precursor.components[:missing+1]
-                            _path = '/'.join(_components[1:])
-                            _output = dlgen.generate_dirindex(_path)
-                            _node = init_node(_parent, _components[-1], _output)
-                            nodes[missing].append((_components, _node))
-                            outputs.append(_output)
-                            _parent = _node
-                        parent = _parent
-                        break
-                if parent:
-                    break
-
+        # Test for duplicate paths
+        paths = [p.path for p in self.precursors]
         try:
-            assert parent
+            assert len(paths) == len(set(paths)), "we got dupes"
         except AssertionError as e:
-            e.args += (depth, precursor.components, nodes[depth-1])
+            e.args += (paths,)
             raise
 
-        if not precursor.output:
-            precursor.output = dlgen.generate_dirindex(precursor.path)
-            outputs.append(precursor.output)
+        # Sort precursors in ascending order of number of path components
+        self.precursors.sort(key=lambda item: len(item.components))
 
-        node = init_node(parent, precursor.name, precursor.output)
-        nodes[depth].append((precursor.components, node))
+        # Create root node for precursor with first (shortest) path
+        root = self.init_node(None, self.precursors[0].name,
+                              self.precursors[0].output)
+        # Indexed by depth, nodes[0] contains only ('/', root).
+        nodes = [[(self.precursors[0].components, root)]]
+        self.precursors.pop(0)
 
-"""Store source directory (with trailing slash) in
-content.metadata['dir'].
+        # Create the rest of the nodes
+        for precursor in self.precursors:
+            depth = len(precursor.components)-1
 
-Some leading components (such as those in PAGE_PATHS for pages) are
-removed.
-"""
-def add_dir(content):
-    global settings
-    if content.source_path is None:
-        return
+            if len(nodes) <= depth:
+                nodes.append([])
 
-    dirname = content.relative_dir
-    if type(content) == contents.Page:
-        dirname = dirname.replace(osp.commonprefix(
-                settings['PAGE_PATHS'] + [dirname,]),
-            '')
+            parent = None
+            for p in nodes[depth-1]:
+                if p[0] == precursor.components[:-1]:
+                    parent = p[1]
 
-    if dirname:
-        # Remove leading slash
-        if osp.isabs(dirname):
-            dirname = osp.relpath(dirname, '/')
-        # Add trailing slash
-        dirname += '/'
+            if not parent:
+                # Go up until we find an ancestor that exists
+                for level in range(depth-1, -1, -1):
+                    for p in nodes[level]:
+                        if p[0] == precursor.components[:level+1]:
+                            # Found an ancestor. The nesting is getting a
+                            # bit ridiculous, but bear with me. Now we're
+                            # going to create a directory listing for each
+                            # missing level.
+                            _parent = p[1]
+                            for missing in range(level+1, depth):
+                                _components = precursor.components[:missing+1]
+                                _path = '/'.join(_components[1:])
+                                _output = self.dlgen.generate_dirindex(_path)
+                                _node = self.init_node(_parent, _components[-1],
+                                                       _output)
+                                nodes[missing].append((_components, _node))
+                                outputs.append(_output)
+                                _parent = _node
+                            parent = _parent
+                            break
+                    if parent:
+                        break
 
-    content.metadata['dir'] = dirname
+            try:
+                assert parent
+            except AssertionError as e:
+                e.args += (depth, precursor.components, nodes[depth-1])
+                raise
 
-def init(pelican):
-    global settings
-    settings = pelican.settings
+            if not precursor.output:
+                precursor.output = self.dlgen.generate_dirindex(
+                    precursor.path)
+                outputs.append(precursor.output)
 
-def add_dirlist_generator(pelican):
-    return DirListGenerator
+            node = self.init_node(parent, precursor.name, precursor.output)
+            nodes[depth].append((precursor.components, node))
 
-def get_dirlist_generator(generator):
-    global dlgen
-    if isinstance(generator, DirListGenerator):
-        dlgen = generator
+    """Store source directory (with trailing slash) in
+    content.metadata['dir'].
 
-def register():
-    signals.initialized.connect(init)
-    signals.get_generators.connect(add_dirlist_generator)
-    signals.generator_init.connect(get_dirlist_generator)
-    signals.all_generators_finalized.connect(add_nodes)
-    signals.content_object_init.connect(add_dir)
+    Some leading components (such as those in PAGE_PATHS for pages) are
+    removed.
+    """
+    def add_dir(self, content):
+        if content.source_path is None:
+            return
+
+        dirname = content.relative_dir
+        if type(content) == contents.Page:
+            dirname = dirname.replace(osp.commonprefix(
+                    self.settings['PAGE_PATHS'] + [dirname,]),
+                '')
+
+        if dirname:
+            # Remove leading slash
+            if osp.isabs(dirname):
+                dirname = osp.relpath(dirname, '/')
+            # Add trailing slash
+            dirname += '/'
+
+        content.metadata['dir'] = dirname
